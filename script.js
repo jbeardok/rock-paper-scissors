9c74f29941d8951dc114d1df47487aca103abb29
James Beard <jbeardok@outlook.com>
2023-01-25 14:11:35 -0600

    Add function for one round of game

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     let int = Math.floor(Math.random() * 3);
     return choices[int];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    if ((playerChoice == 'rock' && computerSelection == 'paper') || 
+        (playerChoice == 'scissors' && computerSelection == 'rock') ||
+        (playerChoice == 'paper') && computerSelection == 'scissors') {
+        return `You lose! ${computerSelection} beats ${playerChoice}!`
+    }
+
+    else if (playerChoice === computerSelection) {
+        return `Draw! Both players have chosen ${computerSelection}!`
+    }
+
+    else {
+        return `You win! ${playerChoice} beats ${computerSelection}!`
+    }
+}
+
+const playerSelection = 'paper';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
