ece4ee0b4e7d47bbd9ae4efb5f6022ce1b3e02ca
James Beard <jbeardok@outlook.com>
2023-03-10 04:44:44 -0600

    Add event listers to trigger playRound()
    Additionally, the DOM is updated with a div element containing the
    results

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const body = document.querySelector('body');
+const div = document.createElement('div');
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     let int = Math.floor(Math.random() * 3);
@@ -5,49 +8,62 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection.toLowerCase();
-    if ((playerChoice == 'rock' && computerSelection == 'paper') || 
-        (playerChoice == 'scissors' && computerSelection == 'rock') ||
-        (playerChoice == 'paper') && computerSelection == 'scissors') {
-        const returnArray = [`You lose! ${computerSelection} beats ${playerChoice}!`, -1];
-        return returnArray;
+    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection == 'rock' && computerSelection == 'paper') || 
+        (playerSelection == 'scissors' && computerSelection == 'rock') ||
+        (playerSelection == 'paper') && computerSelection == 'scissors') {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+            div.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+            body.appendChild(div);
+        // const returnArray = [`You lose! ${computerSelection} beats ${playerSelection}!`, -1];
+        // return returnArray;
     }
 
-    else if (playerChoice === computerSelection) {
-        const returnArray = [`Draw! Both players have chosen ${computerSelection}!`, 0];
-        return returnArray;
-    }
-
-    else {
-        const returnArray = [`You win! ${playerChoice} beats ${computerSelection}!`, 1];
-        return returnArray;
-    }
-}
-
-function game() {
-    let total = 0;
-    let playerSelection;
-    let computerSelection;
-    let resultArray;
-    for (let i=0; i<5; i++) {
-        playerSelection = prompt("Enter a selection:\nRock, Paper, or Scissors: ");
-        computerSelection = getComputerChoice();
-        resultArray = playRound(playerSelection, computerSelection);
-        console.log(resultArray[0])
-        total += resultArray[1];
-    }
-
-    if (total > 0) {
-        return `Congratulations! You have won the game!`;
-    }
-
-    else if (total === 0) {
-        return `The game ended in a draw!`;
+    else if (playerSelection === computerSelection) {
+        console.log(`Draw! ${playerSelection} and ${computerSelection} are the same.`);
+        div.textContent = `Draw! ${playerSelection} and ${computerSelection} are the same.`;
+        body.appendChild(div);
+        // const returnArray = [`Draw! ${playerSelection} and ${computerSelection} are the
+        // const returnArray = [`Draw! Both players have chosen ${computerSelection}!`, 0];
+        // return returnArray;
     }
 
     else {
-        return `Sorry, you have lost the game. Better luck next time!`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        div.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+        body.appendChild(div);
+        // const returnArray = [`You win! ${playerSelection} beats ${computerSelection}!`, 1];
+        // return returnArray;
     }
 }
 
-console.log(game());
+const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {button.addEventListener('click', () => {playRound(button.id, getComputerChoice())})});
+
+// function game() {
+//     let total = 0;
+//     let playerSelection;
+//     let computerSelection;
+//     let resultArray;
+//     for (let i=0; i<5; i++) {
+//         playerSelection = prompt("Enter a selection:\nRock, Paper, or Scissors: ");
+//         computerSelection = getComputerChoice();
+//         resultArray = playRound(playerSelection, computerSelection);
+//         console.log(resultArray[0])
+//         total += resultArray[1];
+//     }
+
+//     if (total > 0) {
+//         return `Congratulations! You have won the game!`;
+//     }
+
+//     else if (total === 0) {
+//         return `The game ended in a draw!`;
+//     }
+
+//     else {
+//         return `Sorry, you have lost the game. Better luck next time!`;
+//     }
+// }
+
+// console.log(game());
