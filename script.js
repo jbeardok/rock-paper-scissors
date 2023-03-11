a142cbf4eb2b1aa24d35cbbeede0d97c6cd2a0c6
James Beard <jbeardok@outlook.com>
2023-03-11 04:34:21 -0600

    Add logic for calculating round results

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const div = document.createElement('div');
-
+const resultPara = document.createElement('p');
+const buttons = document.querySelectorAll('button');
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     let int = Math.floor(Math.random() * 3);
@@ -12,49 +13,49 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'paper') || 
         (playerSelection == 'scissors' && computerSelection == 'rock') ||
         (playerSelection == 'paper') && computerSelection == 'scissors') {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
             div.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
             body.appendChild(div);
+            return -1;
     }
     else if (playerSelection === computerSelection) {
-        console.log(`Draw! ${playerSelection} and ${computerSelection} are the same.`);
         div.textContent = `Draw! ${playerSelection} and ${computerSelection} are the same.`;
         body.appendChild(div);
+        return 0;
     }
     else {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
         div.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
         body.appendChild(div);
+        return 1;
     }
 }
 
-const buttons = document.querySelectorAll('button');
-    buttons.forEach(button => {button.addEventListener('click', () => {playRound(button.id, getComputerChoice())})});
+let computerScore = 0;
+let playerScore = 0;
+let result;
+let round = 0;
+buttons.forEach(button => {button.addEventListener('click', () => 
+{result = playRound(button.id, getComputerChoice());
+    if (result === -1) {computerScore += 1;};
+    if (result === 1) {playerScore += 1;};
+    console.log(computerScore, playerScore);
+    round += 1; console.log(round);
+    if (round === 5) {resultPara.textContent = `Game Over! Player Score: ${playerScore}. Computer Score: ${computerScore}.`;
+        div.appendChild(resultPara);};})});
+
 
 // function game() {
-//     let total = 0;
-//     let playerSelection;
-//     let computerSelection;
-//     let resultArray;
+//     let computerScore;
+//     let playerScore;
+//     let result;
 //     for (let i=0; i<5; i++) {
-//         playerSelection = prompt("Enter a selection:\nRock, Paper, or Scissors: ");
-//         computerSelection = getComputerChoice();
-//         resultArray = playRound(playerSelection, computerSelection);
-//         console.log(resultArray[0])
-//         total += resultArray[1];
+//         buttons.forEach(button => {button.addEventListener('click', () => 
+//         {result = playRound(button.id, getComputerChoice())})});
+//         if (result === -1) {computerScore += 1;}
+//         else if (result === 1) {playerScore += 1;}
+//         else {continue;}
 //     }
-
-//     if (total > 0) {
-//         return `Congratulations! You have won the game!`;
-//     }
-
-//     else if (total === 0) {
-//         return `The game ended in a draw!`;
-//     }
-
-//     else {
-//         return `Sorry, you have lost the game. Better luck next time!`;
-//     }
+//     if (playerScore > computerScore) {console.log('player wins')}
+//     else if (playerScore < computerScore) {console.log('computer wins')}
+//     else {console.log('draw')}
 // }
-
-// console.log(game());
+// game();
